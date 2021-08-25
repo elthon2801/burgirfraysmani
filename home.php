20938d4b1caf891a922da34337b3a888130a2fd6
elthon2801 <62824845+elthon2801@users.noreply.github.com>
2021-08-25 16:21:10 +0800

    Initial commit

---
+++ home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Oswald:400,700">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="styles/home.css">
+</head>
+<body>
+
+
+<nav>
+  <div class="nav-center">
+    <!-- nav header -->
+    <div class="nav-header">
+      <img src="resource/Site/dad.png" class="logo" alt="logo" />
+      <button class="nav-toggle">
+        <i class="fas fa-bars"></i>
+      </button>
+    </div>
+    <!-- links -->
+    <ul class="links">
+      <li>
+        <a href="index.php">Back</a>
+      </li>
+      <li>
+      <li>
+        <a href="home.php">Home</a>
+      </li>
+      <li>
+        <a href="elthon.html">Cayetano</a>
+      </li>
+      <li>
+        <a href="brian.html">Galang</a>
+      </li>
+      <li>
+        <a href="carlos.html">Gonzales</a>
+      </li>
+	  <li>
+        <a href="patrick.html">Pasohil</a>
+      </li>
+	  <li>
+        <a href="joan.html">Lovendino</a>
+      </li>
+	  <li>
+        <a href="ilah.html">Zamora</a>
+      </li>
+    </ul>
+    
+    <!-- social media -->
+    <ul class="social-icons">
+      <li>
+        <a href="https://www.facebook.com/CityCollegeOfAngeles/">
+          <i class="fab fa-facebook"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.twitter.com">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="slideshow">
+  <div class="slideshow-item">
+    <img src="resource/Site/first.jpg" alt="">
+    <div class="slideshow-item-text">
+      <h5>Welcome To Our Page
+      <?php 
+        if(((isset($_POST["login"])) && ($_POST["name"] == "Mr.")) xor  ((isset($_POST['signup'])) && ($_POST['eMail'] == 'nelsonjanperez@cca.edu.ph'))){
+          echo "Engr. Perez</h5>"; } ?>
+      <p>This page is about our personal informations. <br>It is made under the subject Web Programming 2 </p>
+    </div>
+  </div>
+  <div class="slideshow-item">
+    <img src="resource/Site/second.jpg" alt="">
+    <div class="slideshow-item-text">
+      <h5>Welcome To Our Page <?php 
+        if(((isset($_POST["login"])) &&($_POST['name'] == 'Mr.')) xor  ((isset($_POST['signup'])) && ($_POST['eMail'] == 'nelsonjanperez@cca.edu.ph'))){
+          echo "Engr. Perez</h5>"; } ?>
+      <p>This page is about our personal informations. <br>It is made under the subject Web Programming 2 </p>
+    </div>
+  </div>
+  <div class="slideshow-item">
+    <img src="resource/site/Capture.PNG" alt="">
+    <div class="slideshow-item-text">
+      <h5>Welcome To Our Page 
+         <?php 
+        if(((isset($_POST["login"])) &&($_POST['name'] == 'Mr.')) xor  ((isset($_POST['signup'])) && ($_POST['eMail'] == 'nelsonjanperez@cca.edu.ph'))){
+          echo "Engr. Perez</h5>"; } ?>
+      <p>This page is about our personal informations. <br>It is made under the subject Web Programming 2 </p>
+  </div>
+</div>
+</div>
+</body>
+</html> 
